0653770af1caeb586c560e147674458af3bb5c92
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-07-07 09:16:25 +0300

    1 file modified, 1 added

---
+++ components/jobSections/JobWeb.js
@@ -0,0 +1,12 @@
+export default function JobWeb() {
+  return (
+    <div
+      className="bg-white w-full sm:w-1/2  hidden sm:block
+            sm:max-h-[90vh] sm:overflow-y-scroll overflow-x-hidden
+            scrollbar-thin scrollbar-thumb-slate-200 scrollbar-track-slate-50
+            "
+    >
+      merhaba
+    </div>
+  );
+}
