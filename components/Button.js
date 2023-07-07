30ea2c6d576bcd5e4a606e5db11065164f3a92ed
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-07-07 18:55:59 +0300

    2 files modified

--- components/Button.js
+++ components/Button.js
@@ -11,12 +11,11 @@ const Button = (props) => {
     'rounded-lg',
     'py-2',
     'px-4',
-    'font-bold',
+    'font-medium',
     'cursor-pointer',
-    variant === 'primary' ? 'text-white' : 'text-xcolor-900',
-    variant === 'primary' ? 'bg-xcolor-900' : 'bg-slate-200',
-    variant === 'primary' ? 'border-xcolor-900' : 'border-slate-200',
-
+    variant === 'primary' ? 'text-xcolor-100' : 'text-slate-900',
+    variant === 'primary' ? 'bg-slate-950' : 'bg-slate-200',
+    variant === 'primary' ? 'border-slate-900' : 'border-slate-200',
 
     className
   );
@@ -24,23 +23,24 @@ const Button = (props) => {
   return (
     <p className={buttonClasses} onClick={onClick}>
       {children}
-      {
-        arrow ? 
+      {arrow ? (
         <svg
-        class="h-6 w-6 rtl:rotate-180"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
+          class="h-6 w-6 rtl:rotate-180"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
           <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M17 8l4 4m0 0l-4 4m4-4H3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M17 8l4 4m0 0l-4 4m4-4H3"
           />
-        </svg> : ""
-    }
+        </svg>
+      ) : (
+        ''
+      )}
     </p>
   );
 };
