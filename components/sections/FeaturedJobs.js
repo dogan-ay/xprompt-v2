2622f563434ed20d4c8e6f52eed0d5ba2518a700
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-07-11 06:07:02 +0300

    1 file modified

--- components/sections/FeaturedJobs.js
+++ components/sections/FeaturedJobs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NeuButton from '../NeuButton';
 import JobCard from '../JobCard';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const FeaturedJobs = () => {
   return (
@@ -30,37 +31,13 @@ const FeaturedJobs = () => {
       </div>
       <Link href="/">
         <span className="flex justify-center items-center text-xl font-medium  text-slate-800 hover:text-slate-900 hover:bg-slate-200 rounded-lg m-2 py-3 cursor-pointer">
-          <p className="pl-1">Search Jobs</p>
-          <svg
-            viewBox="0 0 24 24"
-            className="w-8 h-8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              {' '}
-              <path
-                d="M16 12L8 12"
-                stroke="#334155"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>{' '}
-              <path
-                d="M13 15L15.913 12.087V12.087C15.961 12.039 15.961 11.961 15.913 11.913V11.913L13 9"
-                stroke="#334155"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>{' '}
-            </g>
-          </svg>
+          <p className="pb-[5px]">Search Jobs</p>
+          <Image
+            src="/icons/chevron-right.svg"
+            width={16}
+            height={16}
+            alt="chevron icon"
+          />
         </span>
       </Link>
     </section>
