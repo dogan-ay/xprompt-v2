58cc24ed43ac1b4ab95f20f972b568af183b1cc5
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-07-13 03:45:50 +0300

    1 file modified

--- components/sections/MainCard.js
+++ components/sections/MainCard.js
@@ -1,11 +1,16 @@
 export const MainCard = () => {
   return (
-    <section className="flex flex-col sm:flex-row bg-sky-50">
-      <div className="bg-slate-200 w-1/2">test</div>
-      <div className="">
-        <h1 className="text-3xl font-bold">Lorem Ipsum Dolor Sit Amet</h1>
-        <p className="Lorem ipsum dolor sit amei amet. Lorem ipum dolor sit amei amet lorem ipsum."></p>
+    <section className="flex flex-col sm:flex-row sm:gap-4 mb-10">
+      <div className="sm:w-96 flex flex-col gap-6 py-4">
+        <h1 className="text-3xl sm:text-4xl font-bold text-slate-950">
+          Lorem Ipsum Dolor Sit Amet
+        </h1>
+        <p className="text-md font-medium text-slate-800">
+          Lorem ipsum dolor sit amei amet. Lorem ipum dolor sit amei amet lorem
+          ipsum.
+        </p>
       </div>
+      <div className="bg-slate-200 grow rounded-lg">test</div>
     </section>
   );
 };
