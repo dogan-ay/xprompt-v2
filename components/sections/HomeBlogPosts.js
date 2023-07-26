10f0352a361d9fe99fa20b98fba93cfcb297c932
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-07-26 19:48:18 +0300

    2 files modified

--- components/sections/HomeBlogPosts.js
+++ components/sections/HomeBlogPosts.js
@@ -7,22 +7,57 @@ import 'swiper/css';
 
 const HomeBlogPosts = () => {
   const { isMobile } = useContext(JobContext);
-
+  const breakpoints = {
+    575: {
+      slidesPerView: 2.8,
+    },
+    525: {
+      slidesPerView: 2.6,
+    },
+    475: {
+      slidesPerView: 2.4,
+    },
+    425: {
+      slidesPerView: 2.2,
+    },
+    375: {
+      slidesPerView: 2,
+    },
+    350: {
+      slidesPerView: 1.8,
+    },
+    325: {
+      slidesPerView: 1.7,
+    },
+    300: {
+      slidesPerView: 1.5,
+    },
+    275: {
+      slidesPerView: 1.4,
+    },
+    250: {
+      slidesPerView: 1.2,
+    },
+  };
   return (
     <>
       {isMobile ? (
         <div className="w-full  my-10">
-          <Swiper classname="w-full" slidesPerView={2.2} spaceBetween={20}>
-          <SwiperSlide>
-            <BlogCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <BlogCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <BlogCard />
-          </SwiperSlide>
-        </Swiper>
+          <Swiper
+            classname="w-full"
+            slidesPerView={1}
+            breakpoints={breakpoints}
+          >
+            <SwiperSlide>
+              <BlogCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <BlogCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <BlogCard />
+            </SwiperSlide>
+          </Swiper>
         </div>
       ) : (
         <section className="lg:grid lg:grid-cols-2 lg:gap-10 flex flex-col gap-8 my-10">
