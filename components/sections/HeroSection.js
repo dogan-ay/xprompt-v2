190a2eab78cb885fa983e1b989d003d1736a195e
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-07-27 07:19:58 +0300

    2 files modified

--- components/sections/HeroSection.js
+++ components/sections/HeroSection.js
@@ -4,8 +4,8 @@ import { JobContext } from '@/contexts/JobContext';
 const HeroSection = () => {
   const { isMobile } = useContext(JobContext);
   return (
-    <section className="flex justify-around items-center gap-8 lg:gap-0 flex-col-reverse my-20 lg:flex-row">
-      <div className="max-w-lg flex flex-col gap-7 ">
+    <section className="flex justify-around items-center gap-8 lg:gap-0 flex-col-reverse my-20 md:flex-row relative">
+      <div className="max-w-lg flex flex-col gap-7 absolute bottom-0 md:static bg-slate-100 md:bg-transparent bg-opacity-25 md:bg-opacity-100 backdrop backdrop-blur-sm md:backdrop-blur-none">
         <h1 className="big-title text-xcolor-900 sm:leading-tight">
           Lorem Ipsum Dolor Amei Sıt Amet
         </h1>
@@ -14,7 +14,7 @@ const HeroSection = () => {
         </p>
         {isMobile ? <NewsInput onlyArrow={true} /> : <NewsInput />}
       </div>
-      <div className="aspect-ratio-4/3 w-60 sm:w-auto sm:max-w-xs md:max-w-sm">
+      <div className="aspect-ratio-4/3 max-w-md md:w-auto md:max-w-xs">
         <img
           className="w-full h-full object-cover"
           src="https://otymerkm.sirv.com/xprompt/hero.png"
