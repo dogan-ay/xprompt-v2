f6f0d83412d3c915efc6592ddb510b6cdbb8a407
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-08-19 19:46:02 +0300

    2 files modified

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,7 +4,7 @@ export default function BlogPage() {
   return (
     <div className="container mx-auto">
       <div className="mx-4 pt-8 sm:m-0">
-        <section className="md:grid md:grid-cols-2 md:gap-10 flex flex-col gap-8 my-10">
+        <section className="sm:grid sm:grid-cols-2 sm:gap-10 flex flex-col gap-8 my-10">
           <PageBlogCard />
           <PageBlogCard />
           <PageBlogCard />
