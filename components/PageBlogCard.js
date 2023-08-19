d85577e29f39ae8f6caa9f0574a10355613197c9
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-08-19 19:28:13 +0300

    1 file modified

--- components/PageBlogCard.js
+++ components/PageBlogCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageBlogCard = () => {
   return (
-    <article className="flex flex-col lg:flex-row lg:items-center gap-10 rounded-2xl bg-white p-3 sm:w-full transition group cursor-pointer sm:hover:scale-[1.01] border border-slate-200">
+    <article className="flex flex-col lg:flex-row lg:items-center gap-4 lg:gap-10 rounded-2xl bg-white p-3 sm:w-full transition group cursor-pointer sm:hover:scale-[1.01] border border-slate-200">
       <div className="h-72 aspect-square max-h-72 w-full lg:w-64 rounded-2xl overflow-hidden">
         <img
           className="w-full h-full object-cover"
@@ -11,7 +11,7 @@ const PageBlogCard = () => {
       </div>
       <div className="flex flex-col gap-4 p-0 ">
         <div className="flex flex-col gap-1">
-          <p className="text-lg sm:text-2xl font-semibold text-slate-900">
+          <p className="text-xl lg:text-2xl font-semibold text-slate-900">
             Lorem Ipsum Dolor
           </p>
           <p className="block text-base text-slate-700 group-hover:text-slate-800">
@@ -21,7 +21,7 @@ const PageBlogCard = () => {
             15/01/2023
           </p>
         </div>
-        <p className="text-sm lg:w-60 md:w-96 w-60 text-slate-700 group-hover:text-slate-800">
+        <p className="text-sm lg:w-60  text-slate-700 group-hover:text-slate-800">
           Lorem İpsum dolor site amet. Lorem İpsum dolor site amet. Lorem İpsum
           dolor site amet.
         </p>
