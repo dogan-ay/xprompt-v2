bf9ad3f60ee733e0e36cbbcbfa4559c1f7bc4321
dogan-ay <72282951+dogan-ay@users.noreply.github.com>
2023-08-21 23:41:07 +0300

    1 file modified

--- components/PageBlogCard.js
+++ components/PageBlogCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const PageBlogCard = () => {
   return (
-    <article className="flex flex-col lg:flex-row lg:items-center lg:gap-10 rounded-2xl bg-white lg:p-3 sm:w-full transition group cursor-pointer sm:hover:scale-[1.01] border border-slate-200">
-      <div className="lg:h-72 aspect-square max-h-72 h-40 w-full lg:w-64 lg:rounded-2xl rounded-t-2xl overflow-hidden">
+    <article className="flex flex-col lg:flex-row lg:items-center lg:gap-10 rounded-2xl bg-white lg:p-3 sm:w-full transition group cursor-pointer sm:hover:scale-[1.01] border border-slate-200 mx-2 sm:mx-0">
+      <div className="lg:h-72 aspect-square max-h-72 h-52 sm:h-40 md:h-52 w-full lg:w-64 lg:rounded-2xl rounded-t-2xl overflow-hidden">
         <img
           className="w-full h-full object-cover"
           src="https://i.pinimg.com/736x/93/34/a0/9334a0802fc144fba9e7a9eba9f198ac.jpg"
         ></img>
       </div>
-      <div className="flex flex-col gap-4 lg:p-0 p-4">
+      <div className="flex flex-col gap-2 lg:gap-4 lg:p-0 p-4">
         <div className="flex flex-col gap-1">
           <p className="text-xl lg:text-2xl font-semibold text-slate-900">
             Lorem Ipsum Dolor
